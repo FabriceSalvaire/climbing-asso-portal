41a6c0e243c4c153fc229248679f70184ed86d5e
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2018-03-27 23:22:19 +0200

    fixed sites.json fixture

--- ClimbingAssoPortal/context_processors.py
+++ ClimbingAssoPortal/context_processors.py
@@ -30,15 +30,15 @@ from django.conf import settings
 def site(request):
 
     ctx = {
-        # "THEME_ADMIN_URL": settings.THEME_ADMIN_URL,
-        "THEME_CONTACT_EMAIL": settings.THEME_CONTACT_EMAIL,
+        # 'THEME_ADMIN_URL': settings.THEME_ADMIN_URL,
+        'THEME_CONTACT_EMAIL': settings.THEME_CONTACT_EMAIL,
     }
 
     if Site._meta.installed:
         site = Site.objects.get_current(request)
         ctx.update({
-            "SITE_NAME": site.name,
-            "SITE_DOMAIN": site.domain
+            'SITE_NAME': site.name,
+            'SITE_DOMAIN': site.domain
         })
 
     return ctx
