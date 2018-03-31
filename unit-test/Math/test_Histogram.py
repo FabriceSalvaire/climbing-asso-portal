8caafc47d75d1eaa0db79d6fde44dc097a747efe
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2018-03-31 21:42:12 +0200

    Added Math

---
+++ unit-test/Math/test_Histogram.py
@@ -0,0 +1,57 @@
+####################################################################################################
+#
+# Climbing Asso Portal - A Portal for Climbing Club (Association)
+# Copyright (C) 2018 Fabrice Salvaire
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################
+
+####################################################################################################
+
+import unittest
+
+####################################################################################################
+
+from ClimbingAssoPortalTools.Math.Binning import Binning1D
+from ClimbingAssoPortalTools.Math.Histogram import *
+
+####################################################################################################
+
+class TestHistogram(unittest.TestCase):
+
+    ##############################################
+
+    def test(self):
+
+        Binning = Binning1D((0, 10), bin_width=1)
+        histogram = Histogram(Binning)
+        histogram.fill(1.5, weight=10)
+        print(histogram)
+
+    ##############################################
+
+    def test_benchmark(self):
+
+        Binning = Binning1D((0, 20), bin_width=1)
+        histogram = Histogram(Binning)
+        for i in range(10000):
+            histogram.fill(np.random.normal(10, 2))
+        print(histogram)
+
+####################################################################################################
+
+if __name__ == '__main__':
+
+    unittest.main()
