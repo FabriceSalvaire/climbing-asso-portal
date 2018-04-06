1e8ab296e95ccd8589d7ff8812d71a3af852b797
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2018-04-07 01:33:25 +0200

    implemented sidebar

--- ClimbingAssoPortal/static-src/src/common/index.js
+++ ClimbingAssoPortal/static-src/src/common/index.js
@@ -1,3 +1,10 @@
 import './bootstrap-js/index.js'
 import './index.scss';
 // import './custom.scss';
+
+
+$(document).ready(function () {
+    $('ul.sidebar-elements li.parent a').on('click', function () {
+	$(this).parent().toggleClass('open');
+    });
+});
